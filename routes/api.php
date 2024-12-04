fd2de5616c7d6d3c9df91218c4a028645fe9a4f8
Jonathan-Ultini <jonathanultini@gmail.com>
2024-12-04 15:33:31 +0100

    set jwt

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Models\User;
+use Tymon\JWTAuth\Facades\JWTAuth;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -15,5 +16,26 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+Route::post('login', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+    ]);
+
+    $credentials = $request->only('email', 'password');
+
+    if ($token = JWTAuth::attempt($credentials)) {
+        return response()->json(['token' => $token]);
+    }
+
+    return response()->json(['error' => 'Unauthorized'], 401);
+});
+
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
